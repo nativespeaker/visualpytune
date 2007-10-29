f78ea162cf29b6793c373c04774683a3f97a5cb7
lanphaday <lanphaday@8e742be5-732e-0410-b276-d7120faee966>
2007-10-29 11:32:21 +0000

    find & replace completed

--- src/codectrl/codepanel_mixin.py
+++ src/codectrl/codepanel_mixin.py
@@ -21,7 +21,8 @@ class FindReplaceMixin(object):
 		
 	def ShowFindReplaceDlg(self):
 		data = wx.FindReplaceData()
-		dlg = wx.FindReplaceDialog(self, data, "Find & Replace", wx.FR_REPLACEDIALOG)
+		dlg = wx.FindReplaceDialog(self, data, \
+			"Find & Replace", wx.FR_REPLACEDIALOG)
 		dlg.data = data  # save a reference to it...
 		dlg.Show(True)
 		
@@ -31,20 +32,40 @@ class FindReplaceMixin(object):
 			start, end = editor.GetSelectionEnd(), editor.GetLength()
 		else:
 			start, end = editor.GetSelectionStart(), 0
-		self.__do_find(editor, start, end, evt.GetFindString(), evt.GetFlags())
-		
+		ret = self.__do_find(editor, start, end, evt.GetFindString(), evt.GetFlags())
+		if ret < 0:
+			wx.MessageDialog(self, \
+				'"%s" not found.'%(evt.GetFindString(), ), \
+				'Find', \
+				wx.OK | wx.ICON_EXCLAMATION).ShowModal()
+				
 	def OnFindNext(self, evt):
 		return self.OnFind(evt)
 		
 	def OnReplace(self, evt):
-		pos = self.OnFind(evt)
-		if pos < 0:
-			return
-		print evt.GetReplaceString()
-		self.GetEditor.ReplaceSelection(evt.GetReplaceString())
+		self.__do_replace(evt)
+		self.OnFind(evt)
 		
 	def OnReplaceAll(self, evt):
-		pass
+		editor = self.GetEditor()
+		cnt = 0
+		while self.__do_find(editor, \
+				0, \
+				editor.GetLength(), \
+				evt.GetFindString(), \
+				evt.GetFlags()) >= 0:
+			cnt += 1
+			self.__do_replace(evt)
+		if cnt == 0:
+			wx.MessageDialog(self, \
+				'"%s" not found.'%(evt.GetFindString(), ), \
+				'Find & Replace', \
+				wx.OK | wx.ICON_EXCLAMATION).ShowModal()
+		else:
+			wx.MessageDialog(self, \
+				'%d substitutions.'%(cnt, ), \
+				'Find & Replace', \
+				wx.OK | wx.ICON_EXCLAMATION).ShowModal()
 		
 	def OnFindReplaceClose(self, evt):
 		evt.GetDialog().Destroy()
@@ -54,9 +75,11 @@ class FindReplaceMixin(object):
 		print pos
 		if pos >= 0:
 			editor.SetSelection(pos, pos + len(findstr))
-		else:
-			wx.MessageDialog(self, \
-				'"%s" not found.'%(findstr, ), \
-				'Find', \
-				wx.OK | wx.ICON_EXCLAMATION).ShowModal()
+			
 		return pos
+		
+	def __do_replace(self, evt):
+		editor = self.GetEditor()
+		sel = editor.GetSelectedText()
+		if sel == evt.GetFindString():
+			editor.ReplaceSelection(evt.GetReplaceString())	
