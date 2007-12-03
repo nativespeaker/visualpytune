b57685a73f44483e9083d7fab51e16c801421ae4
lanphaday <lanphaday@8e742be5-732e-0410-b276-d7120faee966>
2007-12-03 13:13:31 +0000

    added timeit module

---
+++ src/timeit.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*- 
+
+import wx
+
+class TimeitDlg(wx.Dialog):
+	def __init__(self, *a, **k):
+		super(TimeitDlg, self).__init__(*a, **k)
+		
+		panel = wx.Panel(self, wx.NewId())
+		vbox = wx.BoxSizer(wx.VERTICAL)
+		
+		self.SetSizer(vbox)
+		
+def ShowTimeitDlg(parent):
+	dlg = TimeitDlg(parent, wx.NewId(), 'Timeit')
+	dlg.Show()
+	
+if __name__ == '__main__':
+	app = wx.PySimpleApp()
+	ShowTimeitDlg(None)
