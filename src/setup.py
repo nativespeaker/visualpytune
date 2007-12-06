1aca7616013410ffa2b884a8fe14ae10113061f7
lanphaday <lanphaday@8e742be5-732e-0410-b276-d7120faee966>
2007-12-06 06:15:38 +0000

    fixed bug: setup.py can't pack res directory

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,6 @@
 ﻿# -*- coding: utf-8 -*-
 
+from glob import glob
 from distutils.core import setup
 import py2exe
 
@@ -9,4 +10,8 @@ setup(windows=["vpt.py"], \
 	options = {'py2exe':{'optimize':2}}, \
 	name = name, \
 	version = ver, \
+	data_files = [('option', ['option/ui.cfg']), \
+		('res/codectrl', glob('res/codectrl/*.png')), \
+		('res/menu', glob('res/menu/*.png')), \
+		('res/menu', glob('res/toolbar/*.png'))]
 	)
