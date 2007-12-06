5165f2a00893a20f6f6980eb2e4197a0b3dfff81
lanphaday <lanphaday@8e742be5-732e-0410-b276-d7120faee966>
2007-12-06 05:55:27 +0000

    added py2exe pack script added about module

--- trunk/src/vpt.py
+++ trunk/src/vpt.py
@@ -3,10 +3,11 @@
 import wx
 
 import ui
+from about import name, ver
 
 def main():
 	app = wx.PySimpleApp()
-	frame = ui.createUI(None, wx.ID_ANY, 'VisualPyTune', style = wx.DEFAULT_FRAME_STYLE)
+	frame = ui.createUI(None, wx.ID_ANY, name + ' ' + ver, style = wx.DEFAULT_FRAME_STYLE)
 	app.MainLoop()
 	
 if __name__ == '__main__':
