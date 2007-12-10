6b2b8e38a1962d685200eac91b871bd993678a7e
lanphaday <lanphaday@8e742be5-732e-0410-b276-d7120faee966>
2007-12-10 08:26:21 +0000

    added refresh popupmenu to dirctrl

--- src/viewpanel.py
+++ src/viewpanel.py
@@ -12,9 +12,27 @@ def Notebook(parent, *a, **k):
 						dir=UIConfig.inst().getLastDir(), \
 						style = wx.BORDER_NONE)
 	obj.AddPage(parent.dirctrl, 'Files')
+	
+	def OnDirctrlContextMenu(evt):
+		def OnRefresh(evt):
+			path = parent.dirctrl.GetPath()
+			parent.dirctrl.ReCreateTree()
+			parent.dirctrl.SetPath(path)
+			
+		menu = wx.Menu()
+		menu.AppendSeparator()
+		ID_REFRESH = wx.NewId()
+		item_refresh = wx.MenuItem(menu, ID_REFRESH, 'Refresh')
+		obj.Bind(wx.EVT_MENU, OnRefresh, id = ID_REFRESH)
+		menu.AppendItem(item_refresh)
 		
+		parent.dirctrl.PopupMenu(menu)
+		menu.Destroy()
+		
+	parent.dirctrl.Bind(wx.EVT_CONTEXT_MENU, OnDirctrlContextMenu)
+	
 	return obj
-
+	
 def Panel(parent, *a, **k):
 
 	obj = wx.Panel(parent, wx.ID_ANY, *a, **k)
@@ -26,4 +44,4 @@ def Panel(parent, *a, **k):
 	
 	box.Add(obj.notebook, 1, wx.EXPAND | wx.ALL)
 		
-	return obj
+	return obj 
