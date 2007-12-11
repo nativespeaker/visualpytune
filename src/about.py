28265dfb8014f8b689e7a7d9d291bfd7cc8bf3a4
lanphaday <lanphaday@8e742be5-732e-0410-b276-d7120faee966>
2007-12-11 13:08:50 +0000

    修正about模块的版本号

--- src/about.py
+++ src/about.py
@@ -1,7 +1,7 @@
 ﻿# -*- coding:utf-8 -*-
 
 name = 'VisualPyTune'
-ver = '0.1.0'
+ver = '0.1.1'
 author = 'YonghaoLai'
 author_email = 'lanphaday@gmail.com'
 url = 'http://visualpytune.googlecode.com'
