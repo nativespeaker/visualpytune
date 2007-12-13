b046505026ca33932eed5704b594bcf180810626
lanphaday <lanphaday@8e742be5-732e-0410-b276-d7120faee966>
2007-12-13 09:40:41 +0000

    修正 unix 下的打包脚本 暂时去掉了 timeit 的功能

---
+++ src/util.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*- 
+
+import sys, os
+
+def GenCfgPath(*a):
+	exe = sys.executable
+	if exe.endswith('vpt.exe'):
+		return os.path.join(os.path.dirname(sys.executable), *a)
+	else:
+		return os.path.join(*a)
