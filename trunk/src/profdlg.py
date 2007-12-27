a344bd65da8bd24dff6410e4d18c3e1a327d388e
lanphaday <lanphaday@8e742be5-732e-0410-b276-d7120faee966>
2007-12-27 10:10:22 +0000

    增加profile 的集成

---
+++ trunk/src/profdlg.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*- 
+
+import wx
+
+dlgsize = (640, 480)
+
+class ProfDlg(wx.Dialog):
+	def __init__(self, *a, **k):
+		super(ProfDlg, self).__init__(size = dlgsize, *a, **k)
+		
+		vbox = wx.BoxSizer(wx.VERTICAL)
+		
+def ShowTimeitDlg(parent):
+	dlg = ProfDlg(parent, wx.NewId(), 'Profile')
+	dlg.ShowModal()
+	dlg.Destroy()
+	
+if __name__ == '__main__':
+	app = wx.PySimpleApp()
+	ShowTimeitDlg(None)
