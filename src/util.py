cbc9c35bfbd49a37ffb1dbb7f4705f42ffeea12e
lanphaday <lanphaday@8e742be5-732e-0410-b276-d7120faee966>
2008-01-25 08:32:40 +0000

    修正 linux 下 timeit 没有写权限的错误

--- src/util.py
+++ src/util.py
@@ -4,7 +4,14 @@ import sys, os
 
 def GenCfgPath(*a):
 	exe = sys.executable
-	if exe.endswith('vpt.exe'):
+	if exe.endswith('vpt.exe'): # for py2exe
+		return os.path.join(os.path.dirname(sys.executable), *a)
+	else:
+		return os.path.join(os.path.dirname(__file__), *a)
+		
+def GenWritablePath(*a):
+	exe = sys.executable
+	if exe.endswith('vpt.exe'): # for py2exe
 		return os.path.join(os.path.dirname(sys.executable), *a)
 	else:
 		return os.path.join(os.path.dirname(__file__), *a)
